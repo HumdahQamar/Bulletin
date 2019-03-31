45951e52e884ee6bb499d593daf12bf28d473c2c
HumdahQamar <hqmannan@gmail.com>
2019-03-31 19:14:30 +0500

    Complete server setup

--- bulletin/settings.py
+++ bulletin/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'top_five.apps.TopFiveConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
