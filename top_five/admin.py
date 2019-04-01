5105ebcb666ff3e344aa277278b0e289910819ea
HumdahQamar <hqmannan@gmail.com>
2019-04-01 11:42:31 +0500

    Register the Query model with admin

--- top_five/admin.py
+++ top_five/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Query
 
-# Register your models here.
+
+@admin.register(Query)
+class QueryAdmin(admin.ModelAdmin):
+    list_display = ('title', 'timestamp',)
+    search_fields = ('title',)
+    ordering = ('-timestamp',)
